b9b85aafa5c2cae54e96c2e596a4514416e9ff90
Rick Lim <rick.jason.a.lim@gmail.com>
2019-10-29 02:03:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(numbers)
-  new_numbers = [1,2,3]
+  numbers = [1,2,3]
   counter = 0 
   
   while counter < numbers.length.do
-    new_numbers << numbers[counter] ** 2
+    square_array << numbers[counter] ** 2
     counter += 1
   end
  new_numbers
