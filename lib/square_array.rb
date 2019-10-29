637e05e79dd39c8f59339cdb1529c27c0f3f93bf
Rick Lim <rick.jason.a.lim@gmail.com>
2019-10-29 01:25:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-  array = [1,2,3]
+  numbers = [1,2,3]
   counter = 0 
   
   while counter < array.length do
     puts array[counter]
-    counter +-1
+    counter** = 2
   end
 end
 
